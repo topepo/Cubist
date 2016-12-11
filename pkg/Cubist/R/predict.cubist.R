efc10137270e280fa9a7a042e0255d048814b7d0
Max Kuhn <max@Maxs-MacBook-Pro.local>
2016-12-10 19:09:30 -0500

    Updates for issue #1

--- pkg/Cubist/R/predict.cubist.R
+++ pkg/Cubist/R/predict.cubist.R
@@ -1,33 +1,33 @@
 
-predict.cubist <- function (object, newdata = NULL, neighbors = 0, ...) 
-{
-  
+predict.cubist <- function (object, newdata = NULL, neighbors = 0, ...) {
   if(is.null(newdata)) stop("newdata must be non-null")
   
   ## check order of data to make sure that it is the same
   newdata <- newdata[, object$vars$all,drop = FALSE]
-
-
+  
   if(length(neighbors) > 1) stop("only a single value of neighbors is allowed")
   if(neighbors > 9) stop("'neighbors' must be less than 10")
-  if(neighbors > 0)
-    {
-      object$model <- gsub("insts=\"0\"",
-                           paste("insts=\"1\" nn=\"",
-                                 neighbors,
-                                 "\" maxd=\"",
-                                 object$maxd,
-                                 "\"",
-                                 sep = ""),
-                           object$model)
-    }
-
+  if(neighbors > 0){
+    object$model <- gsub("insts=\"0\"",
+                         paste("insts=\"1\" nn=\"",
+                               neighbors,
+                               "\" maxd=\"",
+                               object$maxd,
+                               "\"",
+                               sep = ""),
+                         object$model)
+  }
   
   ## make cases file
   caseString <- makeDataFile(x = newdata, y = NULL)
   
   ## fix breaking predictions when using sample parameter
-  caseModel <- ifelse(!(regexpr("sample", object$model) == -1), paste(substr(object$model, 1, regexpr("sample", object$model)-1), substr(object$model, regexpr("entries", object$model), nchar(object$model)), sep = ""), object$model)
+  caseModel <- ifelse(!(regexpr("sample", object$model) == -1), 
+                      paste0(substr(object$model, 1, 
+                                    regexpr("sample", object$model)-1), 
+                             substr(object$model, regexpr("entries", object$model), 
+                                   nchar(object$model))), 
+                      object$model)
   
   Z <- .C("predictions",
           as.character(caseString),
@@ -36,11 +36,8 @@ predict.cubist <- function (object, newdata = NULL, neighbors = 0, ...)
           as.character(caseModel),
           pred = double(nrow(newdata)),    
           output = character(1),
-          PACKAGE = "Cubist"
-          )
-
-  ## for testing
-  ## cat(Z$output, '\n')
-
+          PACKAGE = "Cubist")
+  
+  
   Z$pred
 }
