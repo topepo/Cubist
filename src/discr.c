fb1367d08573a0bc97da9fbe6461f9757d6ded5b
Max Kuhn <mxkuhn@gmail.com>
2017-11-20 22:08:14 -0500

    Reformat header

--- src/discr.c
+++ src/discr.c
@@ -1,24 +1,24 @@
 /*************************************************************************/
 /*                                                                       */
-/*  Copyright 2010 Rulequest Research Pty Ltd.     */
+/*  Copyright 2010 Rulequest Research Pty Ltd.                           */
 /*                                                                       */
-/*  This file is part of Cubist GPL Edition, a single-threaded version  */
-/*  of Cubist release 2.07.       */
+/*  This file is part of Cubist GPL Edition, a single-threaded version   */
+/*  of Cubist release 2.07.                                              */
 /*                                                                       */
 /*  Cubist GPL Edition is free software: you can redistribute it and/or  */
-/*  modify it under the terms of the GNU General Public License as  */
-/*  published by the Free Software Foundation, either version 3 of the  */
-/*  License, or (at your option) any later version.    */
+/*  modify it under the terms of the GNU General Public License as       */
+/*  published by the Free Software Foundation, either version 3 of the   */
+/*  License, or (at your option) any later version.                      */
 /*                                                                       */
-/*  Cubist GPL Edition is distributed in the hope that it will be  */
+/*  Cubist GPL Edition is distributed in the hope that it will be        */
 /*  useful, but WITHOUT ANY WARRANTY; without even the implied warranty  */
-/*  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  */
-/*  GNU General Public License for more details.    */
+/*  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     */
+/*  GNU General Public License for more details.                         */
 /*                                                                       */
-/*  You should have received a copy of the GNU General Public License  */
-/*  (gpl.txt) along with Cubist GPL Edition.  If not, see   */
+/*  You should have received a copy of the GNU General Public License    */
+/*  (gpl.txt) along with Cubist GPL Edition.  If not, see                */
 /*                                                                       */
-/*      <http://www.gnu.org/licenses/>.      */
+/*      <http://www.gnu.org/licenses/>.                                  */
 /*                                                                       */
 /*************************************************************************/
 
