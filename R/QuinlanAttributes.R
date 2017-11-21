a4616d33df0f2c81312b2da0d48e0e7cd139a419
Max Kuhn <mxkuhn@gmail.com>
2017-11-21 12:41:08 -0500

    Changed arg name

--- R/QuinlanAttributes.R
+++ R/QuinlanAttributes.R
@@ -5,38 +5,38 @@
 #'  
 #' @export
 #' @keywords internal
-#' @param x An object to translate to the format expected by
+#' @param object An object to translate to the format expected by
 #'  the Quinlan C code. 
 #' @return A text representation of the data.
 QuinlanAttributes <-
-  function (x, ...)
+  function (object, ...)
     UseMethod("QuinlanAttributes")
 
 #' @export
-QuinlanAttributes.numeric <- function(x, ...)
+QuinlanAttributes.numeric <- function(object, ...)
   "continuous."
 
 #' @export
 QuinlanAttributes.factor <-
-  function(x, ...)
-    paste(paste(levels(x), collapse = ","), ".", sep = "")
+  function(object, ...)
+    paste(paste(levels(object), collapse = ","), ".", sep = "")
 
 #' @export
 QuinlanAttributes.character <-
-  function(x, ...)
-    paste(paste(unique(x), collapse = ","), ".", sep = "")
+  function(object, ...)
+    paste(paste(unique(object), collapse = ","), ".", sep = "")
 
 #' @export
 QuinlanAttributes.ordered <-
-  function(x, ...)
-    paste("[ordered]", paste(levels(x), collapse = ","), ".", sep = "")
+  function(object, ...)
+    paste("[ordered]", paste(levels(object), collapse = ","), ".", sep = "")
 
 #' @export
 QuinlanAttributes.matrix <-
-  function(x, ...)
-    apply(x, 2, QuinlanAttributes)
+  function(object, ...)
+    apply(object, 2, QuinlanAttributes)
 
 #' @export
 QuinlanAttributes.data.frame <-
-  function(x, ...)
-    unlist(lapply(x,  QuinlanAttributes))
+  function(object, ...)
+    unlist(lapply(object,  QuinlanAttributes))
